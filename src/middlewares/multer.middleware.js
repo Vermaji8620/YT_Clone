4c0d7fba14fc2e522eb99a5627b79fb1cf7fb6a6
vermaji <rajaditya14032002@gmail.com>
2023-11-30 15:23:07 +0530

    writing middleware for cloudinary upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,20 @@
+// ye middlware likhe hai server pe file upload hone ka from the local place so that fir wo cloudinary pe daal sake
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../public/temp");
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    // cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.originalname);
+  },
+});
+
+// creating an instance of the Multer class
+export const upload = multer({ storage: storage });
+
+
+// is 'upload' ko hm fir controller k andr me use kr skte hai...routes likhne k time pe 'upload' se file upload hoga aur fir res.send k andar me response a jayega
