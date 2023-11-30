4a5a185f90f9d650535cc61390c6d97543a88f40
vermaji <rajaditya14032002@gmail.com>
2023-12-01 00:47:44 +0530

    Writing more handler functions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,24 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  // get user details from the frontend
+  // validation
+  // check for already registered user using email and username
+  // check for the images and avatar to be uploaded on cloudinary
+  // create user object and then its entry in database
+  // remove the password and the refresh token from the response
+  // check for the user creation
+  //  return response
+
+  const { username, email, fullName, password } = req.body;
+  if (
+    [fullName, email, password, username].some((field) => field?.trim() === "")
+  ) {
+
+    throw new ApiError(400, "all fields are required")
+
+  }
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,22 @@
-import {Router} from "express";
+import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  // adding the middleware function here..........yaha pe ek baar url pe hit hone se pehle file upload ho jana chchiaye multer k through to the server from the local system
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+
+  registerUser
+);
 
 export default router;
