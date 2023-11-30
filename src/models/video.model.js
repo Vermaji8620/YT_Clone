6388f5489c3dc69bf973d2705aa9b7deb9ee2347
vermaji <rajaditya14032002@gmail.com>
2023-11-30 13:17:19 +0530

    added user and video models

---
+++ src/models/video.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+// The MongoDB Aggregation Framework is a powerful tool for performing data transformations and analysis within MongoDB. It allows you to process and transform data in various ways, including filtering, grouping, sorting, and projecting. Aggregation pipelines consist of a sequence of stages, where each stage performs a specific operation on the data.
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    duration: {
+      type: Number,
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,7 +5,9 @@ const asyncHandler = (fn) => {
     try {
       await fn(req, res, next);
     } catch (error) {
-      res.status(error.code || 500).json({ sucess: false, message: error.message });
+      res
+        .status(error.code || 500)
+        .json({ sucess: false, message: error.message });
     }
   };
 };
